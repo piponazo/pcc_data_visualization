77dd725f0ae1c9e559aed7ec50b2f6424d5cc65a
Luis Díaz Más <piponazo@gmail.com>
2021-11-28 19:06:08 +0100

    Scatter values

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
-plt.style.use('seaborn-dark')
-
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=100)
 
 # Set chart title and labels axes
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of value", fontsize=14)
 
-# Set size of tick labels
-ax.tick_params(axis='both', labelsize=14)
+#set size of tick labels
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
