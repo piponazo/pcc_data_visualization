cd2a00d526620cc60a5c3aad990c1f350e95b942
Luis Díaz Más <piponazo@gmail.com>
2021-11-28 19:12:24 +0100

    Saving data into img + colormaps

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, s=10, c=y_values, cmap=plt.cm.Blues)
 
 # Set chart title and labels axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -18,4 +18,5 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 #set the range for each axis
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+#plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
